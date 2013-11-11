4bc3849de6a2e787252ee5d209c066d99551e31f
Alon <Illniyar@gmail.com>
2013-11-11 09:54:22 +0200

    added test command

--- src/cli.js
+++ src/cli.js
@@ -3,17 +3,14 @@ module.exports.run = function(args,cb) {
     var commandName = args[0]
     var apps = [process.cwd()]
     var promise = moonshine.runCommand(commandName,apps,args)
-	if (cb) return promise.then(cb,cb)
-	promise.then(function(shouldExit,exitMessage){
-		if(shouldExit) {
-			var moonshine = require("./")
-			var logger = moonshine.logFactory();
-			logger.info(exitMessage)
-			process.exit()
-		}
-	},function(err){
-		console.log("process exited with error")
-		console.log(err.stack)
-		process.exit()
-	})
+    if (cb) return promise.then(cb,cb)
+    promise.then(function(shouldExit){
+        if(shouldExit) {
+            process.exit()
+        }
+    },function(err){
+        console.log("process exited with error")
+        console.log(err.stack)
+        process.exit()
+    })
 }
